da341ea05192abf2cae47582b6465e27bb57fac4
Stephanie Brink <brink2@llnl.gov>
2022-07-15 21:19:31 -0700

    update setup.py and add version file
    - remove thicket.tests and thicket.notebooks from packages list
    - add name, url, long_description, version fields
    - add version file

--- setup.py
+++ setup.py
@@ -1,18 +1,37 @@
+# Copyright 2022 Lawrence Livermore National Security, LLC and other
+# Thicket Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: MIT
+
 from setuptools import setup
 from codecs import open
 from os import path
 
-here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the README file
-with open(path.join(here, "README.md"), encoding="utf-8") as f:
-    long_description = f.read()
+def readme():
+    here = path.abspath(path.dirname(__file__))
+    with open(path.join(here, "README.md"), encoding="utf-8") as f:
+        return f.read()
+
+
+# Get the version in a safe way which does not reference thicket `__init__` file
+# per python docs: # https://packaging.python.org/guides/single-sourcing-package-version/
+version = {}
+with open("./thicket/version.py") as fp:
+    exec(fp.read(), version)
+
 
 setup(
+    name="llnl-thicket",
+    version=version["__version__"],
+    license="MIT",
+    description="Toolkit for exploratory data analysis of ensemble performance data",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    keywords="",
+    url="",
     packages=[
         "thicket",
-        "thicket.tests",
-        "thicket.notebooks",
     ],
     install_requires=[
         "llnl-hatchet",
---
+++ thicket/version.py
@@ -0,0 +1,7 @@
+# Copyright 2022 Lawrence Livermore National Security, LLC and other
+# Thicket Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: MIT
+
+__version_info__ = ("2023", "1", "0")
+__version__ = ".".join(__version_info__)
