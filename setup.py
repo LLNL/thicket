5df618982b279df1c7daf8929d7c60c89f085bb4
ilumsden <lumsden.ian@gmail.com>
2023-01-25 11:12:28 -0500

    Re-enables the NPM stuff

--- setup.py
+++ setup.py
@@ -28,8 +28,8 @@ class BuildPyAndNpm(build_py):
 
     def run(self):
         vis_path = path.abspath("./thicket/vis")
-        # subprocess.check_output(["npm", "install", "-y"], cwd=vis_path)
-        # subprocess.check_output(["npm", "run", "build"], cwd=vis_path)
+        subprocess.check_output(["npm", "install", "-y"], cwd=vis_path)
+        subprocess.check_output(["npm", "run", "build"], cwd=vis_path)
         build_py.run(self)
 
 
@@ -40,7 +40,7 @@ setup(
     description="Toolkit for exploratory data analysis of ensemble performance data",
     long_description=readme(),
     long_description_content_type="text/markdown",
-   keywords="",
+    keywords="",
     project_urls={
         "GitHub": "https://github.com/LLNL/thicket",
         "Documentation": "https://thicket.readthedocs.io/",
@@ -61,10 +61,6 @@ setup(
         "pandas >= 1.1",
         "llnl-hatchet",
     ],
-    extras_require={
-        "extrap": ["extrap"]
-    },
-    cmdclass={
-        "build_py": BuildPyAndNpm
-    },
+    extras_require={"extrap": ["extrap"]},
+    cmdclass={"build_py": BuildPyAndNpm},
 )
