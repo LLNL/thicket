677a26e0be3d307a768c69121548fef1388fda78
Michael Richard Mckinsey <mckinsey@catalyst160.llnl.gov>
2023-04-21 12:38:09 -0700

    Refactor file fixtures to use same methodology

--- thicket/tests/conftest.py
+++ thicket/tests/conftest.py
@@ -29,24 +29,23 @@ def example_json(data_dir, tmpdir):
 
 
 @pytest.fixture
-def example_cali(data_dir):
-    """Returns a list of cali profiles"""
+def example_cali(data_dir, tmpdir):
+    files = [
+        "example_all_base_seq_1.cali",
+        "example_all_base_seq_2.cali",
+        "example_all_base_seq_3.cali",
+        "example-profile.cali",
+    ]
     cali_json_dir = os.path.join(data_dir, "example-cali")
-    cali_files = []
-    for i in os.listdir(cali_json_dir):
-        if (
-            os.path.isfile(os.path.join(cali_json_dir, i))
-            and "example_all_base_seq_" in i
-        ):
-            cali_files.append(os.path.join(cali_json_dir, i))
-    cali_files.append(os.path.join(cali_json_dir, "example-profile.cali"))
-
-    return cali_files
+    cali_files = [os.path.join(cali_json_dir, f) for f in files]
+    for f in cali_files:
+        shutil.copy(f, str(tmpdir))
+    return [os.path.join(str(tmpdir), f) for f in files]
 
 
 @pytest.fixture
 def mpi_scaling_cali(data_dir, tmpdir):
-    """Builds a temporary directory containing the mpi scaling files."""
+    """MPI Core scaling study files."""
     files = [
         "cghRRN5HCwJacMr1l_1.cali",  # 27 cores
         "ckFBIeNS6L3ozgKQP_1.cali",  # 64 cores
@@ -55,25 +54,10 @@ def mpi_scaling_cali(data_dir, tmpdir):
         "crPIBPRQJ_nQszMia_1.cali",  # 343 cores
     ]
     mpi_scaling_dir = os.path.join(data_dir, "mpi_scaling_cali")
-    cali_file_1 = os.path.join(mpi_scaling_dir, files[0])
-    cali_file_2 = os.path.join(mpi_scaling_dir, files[1])
-    cali_file_3 = os.path.join(mpi_scaling_dir, files[2])
-    cali_file_4 = os.path.join(mpi_scaling_dir, files[3])
-    cali_file_5 = os.path.join(mpi_scaling_dir, files[4])
-
-    shutil.copy(cali_file_1, str(tmpdir))
-    shutil.copy(cali_file_2, str(tmpdir))
-    shutil.copy(cali_file_3, str(tmpdir))
-    shutil.copy(cali_file_4, str(tmpdir))
-    shutil.copy(cali_file_5, str(tmpdir))
-
-    tmpfile_0 = os.path.join(str(tmpdir), files[0])
-    tmpfile_1 = os.path.join(str(tmpdir), files[1])
-    tmpfile_2 = os.path.join(str(tmpdir), files[2])
-    tmpfile_3 = os.path.join(str(tmpdir), files[3])
-    tmpfile_4 = os.path.join(str(tmpdir), files[4])
-
-    return [tmpfile_0, tmpfile_1, tmpfile_2, tmpfile_3, tmpfile_4]
+    cali_files = [os.path.join(mpi_scaling_dir, f) for f in files]
+    for f in cali_files:
+        shutil.copy(f, str(tmpdir))
+    return [os.path.join(str(tmpdir), f) for f in files]
 
 
 @pytest.fixture
