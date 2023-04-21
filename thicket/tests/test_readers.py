b4b0d74ecc9208e5979bf55a4cfe5ebcf07960df
Michael Richard Mckinsey <mckinsey@catalyst160.llnl.gov>
2023-04-21 14:54:03 -0700

    Update test_from_caliperreader

--- thicket/tests/test_readers.py
+++ thicket/tests/test_readers.py
@@ -15,4 +15,11 @@ def test_from_caliperreader(example_cali):
     """Sanity test a GraphFrame object with known data."""
     th = Thicket.from_caliperreader(example_cali[-1])
 
+    # Check the object type
     assert isinstance(th, Thicket)
+
+    # Check the resultng dataframe shape
+    assert th.dataframe.shape == (24, 7)
+
+    # Check a value in the dataframe
+    assert th.dataframe.loc[th.dataframe.index.get_level_values(0)[0],"Avg time/rank"].values[0] == 0.000082
