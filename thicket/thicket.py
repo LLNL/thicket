fc97b1202abdd85797aeb7a136ccf2f55d70ad39
Stephanie Brink <brink2@llnl.gov>
2023-05-01 15:20:35 -0700

    bugfix: get unique metadata on non-list values
    get_unique_metadata will skip columns where the values are of a list

--- thicket/thicket.py
+++ thicket/thicket.py
@@ -1180,11 +1180,14 @@ class Thicket(GraphFrame):
         """
 
         unique_meta = {}
-        columns = self.metadata.columns
 
-        for col in columns:
-            unique_entries = self.metadata[col].unique().tolist()
-            unique_meta[col] = unique_entries
+        for col in self.metadata.columns:
+            # skip columns where the values are a list
+            if isinstance(self.metadata[col].iloc[0], list):
+                continue
+            else:
+                unique_entries = self.metadata[col].unique().tolist()
+                unique_meta[col] = unique_entries
 
         sorted_meta = dict(sorted(unique_meta.items(), key=lambda x: x[0].lower()))
 
