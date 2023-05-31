0ead5f16cf2162712543c2b094d7e869ef5ceca3
Stephanie Brink <brink2@llnl.gov>
2023-05-31 16:17:37 -0700

    remove outdated stats files, update stats imports in thicket
    calc_average.py       --> mean.py, median.py
    calc_deviation.py     --> std.py, variance.py
    calc_extremum.py      --> minimum.py, maximum.py
    calc_percentiles.py   --> percentiles.py
    calc_corr_nodewise.py --> correlation_nodewise.py

--- thicket/__init__.py
+++ thicket/__init__.py
@@ -7,12 +7,17 @@
 # flake8: noqa: F401
 
 from .thicket import Thicket, InvalidFilter, EmptyMetadataFrame
-from .stats.calc_average import calc_average
-from .stats.calc_corr_nodewise import calc_corr_nodewise
-from .stats.calc_deviation import calc_deviation
-from .stats.calc_extremum import calc_extremum
-from .stats.calc_percentiles import calc_percentile
+from .stats.maximum import maximum
+from .stats.mean import mean
+from .stats.median import median
+from .stats.minimum import minimum
+from .stats.percentiles import percentiles
+from .stats.std import std
+from .stats.variance import variance
+from .stats.calc_boxplot_statistics import calc_boxplot_statistics
+from .stats.correlation_nodewise import correlation_nodewise
 from .stats.check_normality import check_normality
+from .stats.display_boxplot import display_boxplot
 from .stats.display_histogram import display_histogram
 from .stats.display_heatmap import display_heatmap
 from .version import __version__
