17b428dc39e40fdbcc4f7950f408f7e7f44b66c8
Treece Alexander Burgess <burgess9@quartz2306.llnl.gov>
2023-06-02 12:04:07 -0700

    Formatting changes

--- thicket/stats/calc_boxplot_statistics.py
+++ thicket/stats/calc_boxplot_statistics.py
@@ -10,7 +10,7 @@ from ..utils import verify_thicket_structures
 
 
 def calc_boxplot_statistics(thicket, columns=[], quartiles=[0.25, 0.5, 0.75], **kwargs):
-    """Calculate boxplots lowerfence, q1, q2, q3, iqr, upperfence, and outliers for 
+    """Calculate boxplots lowerfence, q1, q2, q3, iqr, upperfence, and outliers for
     each node in the performance data table.
 
     Designed to take in a thicket, and append one or more columns to the aggregated
