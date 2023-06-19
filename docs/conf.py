b3a8d46ecbba953f7bb287ce2ad4aaf529785c65
Stephanie Brink <brink2@llnl.gov>
2023-06-19 11:12:58 -0700

    bump for hotfix release 2023.2.1

--- docs/conf.py
+++ docs/conf.py
@@ -29,9 +29,9 @@ copyright = "2022, Lawrence Livermore National Security, LLC"
 author = "LLNL Developerss"
 
 # The short X.Y version
-version = "2023.2.0"
+version = "2023.2.1"
 # The full version, including alpha/beta/rc tags
-release = "2023.2.0"
+release = "2023.2.1"
 
 
 # -- General configuration ---------------------------------------------------
