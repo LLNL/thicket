1064ea629e706635d0b14a352a120500f9ec177a
Treece Alexander Burgess <burgess9@quartz386.llnl.gov>
2023-06-28 09:24:41 -0700

    Adding unit tests for boxplot, correlation, check normality, and variance

--- thicket/tests/test_stats.py
+++ thicket/tests/test_stats.py
@@ -18,9 +18,9 @@ def test_mean(example_cali):
     th.mean(th_ens, columns=["Min time/rank"])
 
     assert "Min time/rank_mean" in th_ens.statsframe.dataframe.columns
-    # pr #42 needs to be merged before the below  asserts will work
-    # assert "min#inclusive#sum#time.duration_mean" in th_ens.statsframe.exc_metrics + th_ens.statsframe.inc_metrics
-    # assert "min#inclsuive#sum#time.duration_mean" in th_ens.statsframe.show_metric_columns()
+
+    assert "Min time/rank_mean" in th_ens.statsframe.exc_metrics + th_ens.statsframe.inc_metrics
+    assert "Min time/rank_mean" in th_ens.statsframe.show_metric_columns()
 
 
 def test_mean_columnar_join(columnar_join_thicket):
@@ -48,9 +48,9 @@ def test_median(example_cali):
     th.median(th_ens, columns=["Min time/rank"])
 
     assert "Min time/rank_median" in th_ens.statsframe.dataframe.columns
-    # pr #42 needs to be merged before the below  asserts will work
-    # assert "min#inclusive#sum#time.duration_median" in th_ens.statsframe.exc_metrics + th_ens.statsframe.inc_metrics
-    # assert "min#inclsuive#sum#time.duration_median" in th_ens.statsframe.show_metric_columns()
+
+    assert "Min time/rank_median" in th_ens.statsframe.exc_metrics + th_ens.statsframe.inc_metrics
+    assert "Min time/rank_median" in th_ens.statsframe.show_metric_columns()
 
 def test_median_columnar_join(columnar_join_thicket):
     thicket_list, thicket_list_cp, combined_th = columnar_join_thicket
@@ -77,9 +77,9 @@ def test_minimum(example_cali):
     th.minimum(th_ens, columns=["Min time/rank"])
 
     assert "Min time/rank_min" in th_ens.statsframe.dataframe.columns
-    # pr #42 needs to be merged before the below  asserts will work
-    # assert "min#inclusive#sum#time.duration_minimum" in th_ens.statsframe.exc_metrics + th_ens.statsframe.inc_metrics
-    # assert "min#inclsuive#sum#time.duration_minimum" in th_ens.statsframe.show_metric_columns()
+
+    assert "Min time/rank_min" in th_ens.statsframe.exc_metrics + th_ens.statsframe.inc_metrics
+    assert "Min time/rank_min" in th_ens.statsframe.show_metric_columns()
 
 
 def test_minimum_columnar_join(columnar_join_thicket):
@@ -107,6 +107,13 @@ def test_maximum(example_cali):
     th.maximum(th_ens, columns=["Min time/rank"])
 
     assert "Min time/rank_max" in th_ens.statsframe.dataframe.columns
+
+    assert "Min time/rank_max" in th_ens.statsframe.exc_metrics + th_ens.statsframe.inc_metrics
+    assert "Min time/rank_max" in th_ens.statsframe.show_metric_columns()
+
+
+def test_maximum_columnar_join(columnar_join_thicket):
+
     # pr #42 needs to be merged before the below  asserts will work
     # assert "min#inclusive#sum#time.duration_minimum" in th_ens.statsframe.exc_metrics + th_ens.statsframe.inc_metrics
     # assert "min#inclsuive#sum#time.duration_minimum" in th_ens.statsframe.show_metric_columns()
@@ -137,10 +144,9 @@ def test_std(example_cali):
     th.std(th_ens, columns=["Min time/rank"])
 
     assert "Min time/rank_std" in th_ens.statsframe.dataframe.columns
-    # pr #42 needs to be merged before the below asserts will work
-    # assert "min#inclusive#sum#time.duration_std" in th_ens.statsframe.exc_metrics + th_ens.statsframe.inc_metrics
-    # assert "min#inclsuive#sum#time.duration_std" in th_ens.statsframe.show_metric_columns()
 
+    assert "Min time/rank_std" in th_ens.statsframe.exc_metrics + th_ens.statsframe.inc_metrics
+    assert "Min time/rank_std" in th_ens.statsframe.show_metric_columns()
 
 def test_std_columnar_join(columnar_join_thicket):
     thicket_list, thicket_list_cp, combined_th = columnar_join_thicket
@@ -149,7 +155,7 @@ def test_std_columnar_join(columnar_join_thicket):
 
     assert list(combined_th.statsframe.dataframe.columns) == [("", "name")]
 
-    # th.std(combined_th. columns=[(idx, "min#inclusive#sum#time.duration")])
+    # th.std(combined_th, columns=[(idx, "min#inclusive#sum#time.duration")])
 
     # assert (idx, "min#inclusive#sum#time.duration_std") in combined_th.statsframe.dataframe.columns
     # pr #42 needs to be merged before the below asserts will work
@@ -167,10 +173,9 @@ def test_percentiles(example_cali):
 
     assert "Min time/rank_percentiles" in th_ens.statsframe.dataframe.columns
     assert len(th_ens.statsframe.dataframe["Min time/rank_percentiles"][0]) == 3
-    # pr #42 needs to be merged before the below asserts will work
-    # assert "min#inclusive#sum#time.duration_percentiles" in th_ens.statsframe.exc_metrics + th_ens.statsframe.inc_metrics
-    # assert "min#inclsuive#sum#time.duration_percentiles" in th_ens.statsframe.show_metric_columns()
 
+    assert "Min time/rank_percentiles" in th_ens.statsframe.exc_metrics + th_ens.statsframe.inc_metrics
+    assert "Min time/rank_percentiles" in th_ens.statsframe.show_metric_columns()
 
 def test_percentiles_columnar_join(columnar_join_thicket):
     thicket_list, thicket_list_cp, combined_th = columnar_join_thicket
@@ -179,9 +184,146 @@ def test_percentiles_columnar_join(columnar_join_thicket):
 
     assert list(combined_th.statsframe.dataframe.columns) == [("", "name")]
 
-    # th.std(combined_th. columns=[(idx, "min#inclusive#sum#time.duration")])
+    # th.percentiles(combined_th, columns=[(idx, "min#inclusive#sum#time.duration")
 
     # assert (idx, "min#inclusive#sum#time.duration_percentiles") in combined_th.statsframe.dataframe.columns
     # pr #42 needs to be merged before the below asserts will work
     # assert (idx, "min#inclusive#sum#time.duration_percentiles") in combined_th.statsframe.exc_metrics + th_ens.statsframe.inc_metrics
     # assert (idx, "min#inclusive#sum#time.duration_percentiles") in combined_th.stataframe.show_metric_columns()
+
+def test_variance(example_cali):
+    th_ens = th.Thicket.from_caliperreader(example_cali)
+
+    assert sorted(th_ens.dataframe.index.get_level_values(0).unique()) == sorted(th_ens.statsframe.dataframe.index.values)
+
+    assert list(th_ens.statsframe.dataframe.columns) == ["name"]
+
+    th.variance(th_ens, columns=["Min time/rank"])
+
+    assert "Min time/rank_var" in th_ens.statsframe.dataframe.columns
+    assert "Min time/rank_var" in th_ens.statsframe.exc_metrics + th_ens.statsframe.inc_metrics
+    assert "Min time/rank_var" in th_ens.statsframe.show_metric_columns()
+
+
+def test_variance_columnar_join(columnar_join_thicket):
+    thicket_list, thicket_list_cp, combined_th = columnar_join_thicket
+    idx = combined_th.dataframe.columns.levels[0][0]
+    assert sorted(combined_th.dataframe.index.get_level_values(0).unique()) == sorted(combined_th.statsframe.dataframe.index.values)
+
+    assert list(combined_th.statsframe.dataframe.columns) == [("", "name")]
+
+    # th.variance(combined_th. columns=[(idx, "min#inclusive#sum#time.duration")])
+
+    # assert (idx, "min#inclusive#sum#time.duration_std") in combined_th.statsframe.dataframe.columns
+    # pr #42 needs to be merged before the below asserts will work
+    # assert (idx, "min#inclusive#sum#time.duration_std") in combined_th.statsframe.exc_metrics + th_ens.statsframe.inc_metrics
+    # assert (idx, "min#inclusive#sum#time.duration_std") in combined_th.stataframe.show_metric_columns()
+
+
+def test_normality(example_cali):
+    th_ens = th.Thicket.from_caliperreader(example_cali)
+
+    assert sorted(th_ens.dataframe.index.get_level_values(0).unique()) == sorted(th_ens.statsframe.dataframe.index.values)
+
+    assert list(th_ens.statsframe.dataframe.columns) == ["name"]
+
+    th.check_normality(th_ens, columns=["Min time/rank"])
+
+    assert th_ens.statsframe.dataframe["Min time/rank_normality"][0] in {"True", "False"}
+
+    assert "Min time/rank_normality" in th_ens.statsframe.dataframe.columns
+    assert "Min time/rank_normality" in th_ens.statsframe.exc_metrics + th_ens.statsframe.inc_metrics
+    assert "Min time/rank_normality" in th_ens.statsframe.show_metric_columns()
+
+
+def test_normality_columnar_join(columnar_join_thicket):
+    thicket_list, thicket_list_cp, combined_th = columnar_join_thicket
+    idx = combined_th.dataframe.columns.levels[0][0]
+    assert sorted(combined_th.dataframe.index.get_level_values(0).unique()) == sorted(combined_th.statsframe.dataframe.index.values)
+
+    assert list(combined_th.statsframe.dataframe.columns) == [("", "name")]
+
+    # th.check_normality(combined_th. columns=[(idx, "min#inclusive#sum#time.duration")])
+
+    # assert (idx, "min#inclusive#sum#time.duration_std") in combined_th.statsframe.dataframe.columns
+    # pr #42 needs to be merged before the below asserts will work
+    # assert (idx, "min#inclusive#sum#time.duration_std") in combined_th.statsframe.exc_metrics + th_ens.statsframe.inc_metrics
+    # assert (idx, "min#inclusive#sum#time.duration_std") in combined_th.stataframe.show_metric_columns()
+
+
+def test_correlation(example_cali):
+    th_ens = th.Thicket.from_caliperreader(example_cali)
+
+    assert sorted(th_ens.dataframe.index.get_level_values(0).unique()) == sorted(th_ens.statsframe.dataframe.index.values)
+
+    assert list(th_ens.statsframe.dataframe.columns) == ["name"]
+
+    th.correlation_nodewise(th_ens, column1="Min time/rank", column2="Max time/rank", correlation="pearson")
+
+    assert "Min time/rank_vs_Max time/rank pearson" in th_ens.statsframe.dataframe.columns
+
+
+def test_correlation_columnar_join(columnar_join_thicket):
+    thicket_list, thicket_list_cp, combined_th = columnar_join_thicket
+    idx = combined_th.dataframe.columns.levels[0][0]
+    assert sorted(combined_th.dataframe.index.get_level_values(0).unique()) == sorted(combined_th.statsframe.dataframe.index.values)
+
+    assert list(combined_th.statsframe.dataframe.columns) == [("", "name")]
+
+    # th.correlation_nodewise(combined_th. columns=[(idx, "min#inclusive#sum#time.duration")])
+
+    # assert (idx, "min#inclusive#sum#time.duration_std") in combined_th.statsframe.dataframe.columns
+    # pr #42 needs to be merged before the below asserts will work
+    # assert (idx, "min#inclusive#sum#time.duration_std") in combined_th.statsframe.exc_metrics + th_ens.statsframe.inc_metrics
+    # assert (idx, "min#inclusive#sum#time.duration_std") in combined_th.stataframe.show_metric_columns()
+
+
+def test_boxplot(example_cali):
+    th_ens = th.Thicket.from_caliperreader(example_cali)
+
+    assert sorted(th_ens.dataframe.index.get_level_values(0).unique()) == sorted(th_ens.statsframe.dataframe.index.values)
+
+    assert list(th_ens.statsframe.dataframe.columns) == ["name"]
+          
+    th.calc_boxplot_statistics(th_ens, columns=["Min time/rank"], quartiles=[0.25, 0.50, 0.75])
+          
+    assert "Min time/rank_q1(0.25, 0.5, 0.75)" in th_ens.statsframe.dataframe.columns
+    assert "Min time/rank_q2(0.25, 0.5, 0.75)" in th_ens.statsframe.dataframe.columns
+    assert "Min time/rank_q3(0.25, 0.5, 0.75)" in th_ens.statsframe.dataframe.columns
+    assert "Min time/rank_iqr(0.25, 0.5, 0.75)" in th_ens.statsframe.dataframe.columns
+    assert "Min time/rank_lowerfence(0.25, 0.5, 0.75)" in th_ens.statsframe.dataframe.columns
+    assert "Min time/rank_upperfence(0.25, 0.5, 0.75)" in th_ens.statsframe.dataframe.columns
+    assert "Min time/rank_outliers(0.25, 0.5, 0.75)" in th_ens.statsframe.dataframe.columns
+
+    assert "Min time/rank_q1(0.25, 0.5, 0.75)" in th_ens.statsframe.exc_metrics + th_ens.statsframe.inc_metrics
+    assert "Min time/rank_q2(0.25, 0.5, 0.75)" in th_ens.statsframe.exc_metrics + th_ens.statsframe.inc_metrics
+    assert "Min time/rank_q3(0.25, 0.5, 0.75)" in th_ens.statsframe.exc_metrics + th_ens.statsframe.inc_metrics
+    assert "Min time/rank_iqr(0.25, 0.5, 0.75)" in th_ens.statsframe.exc_metrics + th_ens.statsframe.inc_metrics
+    assert "Min time/rank_lowerfence(0.25, 0.5, 0.75)" in th_ens.statsframe.exc_metrics + th_ens.statsframe.inc_metrics
+    assert "Min time/rank_upperfence(0.25, 0.5, 0.75)" in th_ens.statsframe.exc_metrics + th_ens.statsframe.inc_metrics
+    assert "Min time/rank_outliers(0.25, 0.5, 0.75)" in th_ens.statsframe.exc_metrics + th_ens.statsframe.inc_metrics
+
+    assert "Min time/rank_q1(0.25, 0.5, 0.75)" in th_ens.statsframe.show_metric_columns()
+    assert "Min time/rank_q2(0.25, 0.5, 0.75)" in th_ens.statsframe.show_metric_columns()
+    assert "Min time/rank_q3(0.25, 0.5, 0.75)" in th_ens.statsframe.show_metric_columns()
+    assert "Min time/rank_iqr(0.25, 0.5, 0.75)" in th_ens.statsframe.show_metric_columns()
+    assert "Min time/rank_lowerfence(0.25, 0.5, 0.75)" in th_ens.statsframe.show_metric_columns()
+    assert "Min time/rank_upperfence(0.25, 0.5, 0.75)" in th_ens.statsframe.show_metric_columns()
+    assert "Min time/rank_outliers(0.25, 0.5, 0.75)" in th_ens.statsframe.show_metric_columns()
+        
+
+def test_boxplot_columnar_join(columnar_join_thicket):
+    thicket_list, thicket_list_cp, combined_th = columnar_join_thicket
+    idx = combined_th.dataframe.columns.levels[0][0]
+    assert sorted(combined_th.dataframe.index.get_level_values(0).unique()) == sorted(combined_th.statsframe.dataframe.index.values)
+    
+    assert list(combined_th.statsframe.dataframe.columns) == [("", "name")]
+
+    # th.calc_boxplot_statistics(combined_th. columns=[(idx, "min#inclusive#sum#time.duration")])
+    
+    # assert (idx, "min#inclusive#sum#time.duration_std") in combined_th.statsframe.dataframe.columns
+    # pr #42 needs to be merged before the below asserts will work
+    # assert (idx, "min#inclusive#sum#time.duration_std") in combined_th.statsframe.exc_metrics + th_ens.statsframe.inc_metrics
+    # assert (idx, "min#inclusive#sum#time.duration_std") in combined_th.stataframe.show_metric_columns()
+=======
+>>>>>>> 74c3ff0bfbcab85a982db09954ef4dcb638122e5
