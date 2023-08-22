99467fb45f1aa6733ef56806e893e47ce57f4561
Marcus Ritter <ritter5@llnl.gov>
2023-08-21 17:53:03 -0700

    updated produce_models() in model_extrap.py to enable measurement repetition support

---
+++ UPDATES.md
@@ -0,0 +1,4 @@
+UPDATES:
+
+* added a check for .cali file type to reader_dispatch() so no value error is raised when other file types, folders are in the provided dir
+* refactored the code in produce_models() so it can deal with measurement repetitions
--- thicket/model_extrap.py
+++ thicket/model_extrap.py
@@ -11,9 +11,9 @@ import extrap.entities as xent
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from extrap.entities.experiment import (
-    Experiment,
-)  # For some reason it errors if "Experiment" is not explicitly imported
+#from extrap.entities.experiment import (
+#    Experiment,
+#)  # For some reason it errors if "Experiment" is not explicitly imported
 from extrap.fileio import io_helper
 from extrap.modelers.model_generator import ModelGenerator
 
@@ -52,7 +52,10 @@ class ModelWrapper:
 
         # Scatter plot
         params = [ms.coordinate[0] for ms in measures_sorted]  # X values
-        measures = [ms.value(True) for ms in measures_sorted]  # Y values
+        median = [ms.median for ms in measures_sorted]
+        mean = [ms.mean for ms in measures_sorted] # Y values
+        mins = [ms.minimum for ms in measures_sorted]
+        maxes = [ms.maximum for ms in measures_sorted]
 
         # Line plot
 
@@ -71,15 +74,23 @@ class ModelWrapper:
         ax.plot(x_vals, y_vals, label=self.mdl.hypothesis.function)
 
         # Plot scatter
-        ax.plot(params, measures, "ro", label=self.mdl.callpath)
+        
+        errors = [
+            np.subtract(mean, mins),
+            np.subtract(maxes, mean),
+        ]
+        
+        #ax.plot(params, measures, "ro", yerr=[min,maxs], label=self.mdl.callpath)
+        ax.errorbar(params, mean, yerr=errors, fmt=".k", label=self.mdl.callpath)
+        ax.plot(params, median, "+k", label="median")
 
         ax.set_xlabel(self.param_name)
         ax.set_ylabel(self.mdl.metric)
         if RSS:
             ax.text(
                 x_vals[0],
-                max(y_vals + measures),
-                "RSS = " + self.mdl.hypothesis.RSS,
+                max(y_vals + mean),
+                "RSS = " + str(self.mdl.hypothesis.RSS),
             )
         ax.legend(loc=1)
 
@@ -108,11 +119,15 @@ class Modeling:
         """
         self.tht = tht
         self.param_name = param_name
+        #debug
+        print("self.param_name:",self.param_name)
 
         # Assign param
         # Get params from metadata table
         if not params:
             self.params = self.tht.metadata[param_name].tolist()
+            # remove duplicates from list
+            self.params = list(set(self.params))
         # params must be provided by the user
         else:
             if not isinstance(params, dict):
@@ -194,93 +209,115 @@ class Modeling:
             add_stats (bool): Option to add hypothesis function statistics to the
                 aggregated statistics table
         """
-        # Setup domain values one time. Have to match ordering with range
-        # values (i.e. performance data table profile ordering)
-        param_coords = []  # default coordinates for all profiles
-
+        
+        # create an extra-p experiment
+        from extrap.entities.experiment import Experiment
+        experiment = Experiment()
+        
+        # create the model parameters
+        #NOTE: implementation does not work for multiple model parameters
+        from extrap.entities.parameter import Parameter
+        model_parameter = Parameter(self.param_name)
+        experiment.add_parameter(model_parameter)
+        
         # Mapping from metadata profiles to the parameter
         meta_param_mapping = self.tht.metadata[self.param_name].to_dict()
-
-        # Flipped version of mapping dictionary
-        meta_param_mapping_flipped = dict(
-            [(value, key) for key, value in meta_param_mapping.items()]
-        )
-
+        
         # Ordering of profiles in the performance data table
         ensemble_profile_ordering = list(self.tht.dataframe.index.unique(level=1))
-
-        # Append coordinates in order
+        
+        # create the measurement coordinates
+        #NOTE: implementation does not work for multiple model parameters
+        from extrap.entities.coordinate import Coordinate
         for profile in ensemble_profile_ordering:
-            param_coords.append(
-                xent.coordinate.Coordinate(float(meta_param_mapping[profile]))
-            )
-
-        # Iterate over nodes (outer index)
-        for node, single_node_df in self.tht.dataframe.groupby(level=0):
-            # Start experiment
-            exp = Experiment()
-            exp.add_parameter(xent.parameter.Parameter(self.param_name))
-
-            # For all chosen metrics
-            for met in self.chosen_metrics:
-                measures = []
-                # Iterate over profiles (secondary index)
-                for profile, single_prof_df in single_node_df.groupby(level=1):
-                    measures.append(single_prof_df[met].tolist())
-                # Apply aggregation function to measurements
-                for i in range(len(measures)):
-                    # Only apply aggregation function if more that one value
-                    if isinstance(measures[i], list):
-                        measures[i] = agg_func(measures[i])
-                # Add coordinates (points at which measurements were taken) for the
-                # default case
-                if len(measures) == len(param_coords):
-                    exp.coordinates.extend(param_coords)
-                # Handle case where profile(s) do not contain a measurement for the
-                # current node
-                elif len(measures) < len(param_coords):
-                    param_coords_subset = []
-                    df_index = self.tht.dataframe.loc[node, met].index
-                    for coord in param_coords:
-                        profile = meta_param_mapping_flipped[coord._values[0]]
-                        if df_index.isin([profile], level=0).any():
-                            param_coords_subset.append(coord)
-                        else:
-                            print(
-                                "(Coordinate removed) Measurement at ("
-                                + profile
-                                + "): "
-                                + meta_param_mapping[profile]
-                                + "DNE for node "
-                                + node
-                            )
-                    exp.coordinates.extend(param_coords_subset)
-
-                # Create metric object
-                metric_obj = xent.metric.Metric(met)
-                exp.add_metric(xent.metric.Metric(met))
-                # Create callpath object and call tree
-                cpath = xent.callpath.Callpath(node.frame["name"])
-                exp.add_callpath(cpath)
-                exp.call_tree = io_helper.create_call_tree(exp.callpaths)
-                # Add measurement objects to experiment
-                for coord, measurement in zip(exp.coordinates, measures):
-                    measurement_obj = xent.measurement.Measurement(
-                        coord, cpath, metric_obj, measurement
-                    )
-                    exp.add_measurement(measurement_obj)
-                # Sanity check
-                io_helper.validate_experiment(exp)
-                # Generate model
-                model_gen = ModelGenerator(exp)
-                model_gen.model_all()
-                mkey = (cpath, metric_obj)
-                self.tht.statsframe.dataframe.at[node, met + MODEL_TAG] = ModelWrapper(
-                    model_gen.models[mkey], self.param_name
-                )
-                # Add statistics to aggregated statistics table
-                if add_stats:
-                    self._add_extrap_statistics(node, met)
+            if Coordinate(float(meta_param_mapping[profile])) not in experiment.coordinates:
+                experiment.add_coordinate(Coordinate(float(meta_param_mapping[profile])))
+        # debug
+        print("coordinates:",experiment.coordinates)
+        print("len coordinates:",len(experiment.coordinates))
+            
+        # create the callpaths
+        #NOTE: could add calltree later on, possibly from hatchet data if available
+        from extrap.entities.callpath import Callpath
+        for node, _ in self.tht.dataframe.groupby(level=0):
+            if Callpath(node.frame["name"]) not in experiment.callpaths:
+                experiment.add_callpath(Callpath(node.frame["name"]))
+        # debug 
+        print("Callpaths:",experiment.callpaths)
+        
+        # create the metrics
+        from extrap.entities.metric import Metric
+        for metric in self.chosen_metrics:
+            experiment.add_metric(Metric(metric))
+        # debug
+        print("Metrics:",experiment.metrics)
+        
+        # iteratre over coordinates
+        for coordinate in experiment.coordinates:
+            # iterate over callpaths
+            for callpath in experiment.callpaths:
+                # iterate over metrics
+                for metric in experiment.metrics:
+                    # iterate over measurements
+                    #TODO: figure out how to access these group by data frames directly
+                    # to remove these loops...
+                    values = []
+                    for node, single_node_df in self.tht.dataframe.groupby(level=0):
+                        if Callpath(node.frame["name"]) == callpath:
+                            for profile, single_prof_df in single_node_df.groupby(level=1):
+                                if Coordinate(float(meta_param_mapping[profile])) == coordinate:
+                                    # if no data is found for this config, do not anything
+                                    try:
+                                        value = single_prof_df[str(metric)].tolist()
+                                        if len(value) == 1:
+                                            values.append(value[0])
+                                    except Exception as e:
+                                        print("Could not add measured value for:", 
+                                              str(callpath), 
+                                              str(metric), 
+                                              str(coordinate),
+                                              ". See exception:", e)
+                    from extrap.entities.measurement import Measurement
+                    # if there was no data found at all for this config, do not add any measurement to the experiment
+                    if len(values) > 0:
+                        experiment.add_measurement(Measurement(coordinate, callpath, metric, values))
+                    else:
+                        print("Could not add measurement values for:",
+                              str(callpath), 
+                              str(metric), 
+                              str(coordinate),
+                              ". No measured values found for this particular configuration.")
+                        
+                    # debug
+                    print("DEBUG:", str(coordinate), str(callpath), str(metric), values)
+            
+        # debug
+        print("Measurements:",experiment.measurements)
+        
+        # create the calltree based on the callpaths
+        #TODO: could pip actual calltree in here...
+        from extrap.fileio.io_helper import create_call_tree
+        experiment.call_tree = create_call_tree(experiment.callpaths)
+                    
+        # check the created experiment for its validty
+        io_helper.validate_experiment(experiment)
+            
+        # generate models using Extra-P model generator
+        model_gen = ModelGenerator(experiment)
+        model_gen.model_all()
+        
+        # add the models, and statistics into the dataframe
+        for callpath in experiment.callpaths:
+            for metric in experiment.metrics:
+                mkey = (callpath, metric)
+                for node, _ in self.tht.dataframe.groupby(level=0):
+                    if Callpath(node.frame["name"]) == callpath:
+                        self.tht.statsframe.dataframe.at[node, str(metric) + MODEL_TAG] = ModelWrapper(
+                            model_gen.models[mkey], self.param_name
+                        )
+                        # Add statistics to aggregated statistics table
+                        if add_stats:
+                            self._add_extrap_statistics(node, str(metric))
 
     def _componentize_function(model_object):
         """Componentize one Extra-P modeling object into a dictionary of its parts
