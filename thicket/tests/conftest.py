b2942ffc8f32f8b528dc9b1f11410c4743209577
Michael Richard McKinsey <mckinsey@login4.cluster>
2023-09-06 12:13:57 -0500

    Update stats test function naming

--- thicket/tests/conftest.py
+++ thicket/tests/conftest.py
@@ -21,7 +21,7 @@ def thicket_axis_columns(mpi_scaling_cali, rajaperf_basecuda_xl_cali):
 
     Returns:
         list: List of original thickets, list of deepcopies of original thickets, and
-            columnar-joined thicket.
+            column-joined thicket.
     """
     th_mpi_1 = Thicket.from_caliperreader(mpi_scaling_cali[0:2])
     th_mpi_2 = Thicket.from_caliperreader(mpi_scaling_cali[2:4])
@@ -53,16 +53,15 @@ def thicket_axis_columns(mpi_scaling_cali, rajaperf_basecuda_xl_cali):
 
 
 @pytest.fixture
-def stats_columnar_join_thicket(rajaperf_basecuda_xl_cali):
-    """Generator for 'columnar_join' thicket for test_stats.py.
+def stats_thicket_axis_columns(rajaperf_basecuda_xl_cali):
+    """Generator for 'concat_thickets(axis="columns")' thicket for test_stats.py.
 
     Arguments:
-        mpi_scaling_cali (list): List of Caliper files for MPI scaling study.
         rajaperf_basecuda_xl_cali (list): List of Caliper files for base cuda variant.
 
     Returns:
         list: List of original thickets, list of deepcopies of original thickets, and
-            columnar-joined thicket.
+            column-joined thicket.
     """
     th_cuda128_1 = Thicket.from_caliperreader(rajaperf_basecuda_xl_cali[0:4])
     th_cuda128_2 = Thicket.from_caliperreader(rajaperf_basecuda_xl_cali[5:9])
@@ -70,15 +69,16 @@ def stats_columnar_join_thicket(rajaperf_basecuda_xl_cali):
     # To check later if modifications were unexpectedly made
     th_cuda128_1_deep = th_cuda128_1.deepcopy()
     th_cuda128_2_deep = th_cuda128_2.deepcopy()
-    thicket_list = [th_cuda128_1, th_cuda128_2]
-    thicket_list_cp = [th_cuda128_1_deep, th_cuda128_2_deep]
+    thickets = [th_cuda128_1, th_cuda128_2]
+    thickets_cp = [th_cuda128_1_deep, th_cuda128_2_deep]
 
-    combined_th = Thicket.columnar_join(
-        thicket_list=thicket_list,
-        header_list=["Cuda 1", "Cuda 2"],
+    combined_th = Thicket.concat_thickets(
+        thickets=thickets,
+        axis="columns",
+        headers=["Cuda 1", "Cuda 2"],
     )
 
-    return thicket_list, thicket_list_cp, combined_th
+    return thickets, thickets_cp, combined_th
 
 
 @pytest.fixture
