9a3cf41bb0a6743b96a8988aa2d59ede14e74c3d
Andrew Mueller <amuell11@vols.utk.edu>
2023-11-27 08:58:03 -0800

    Changing some logic

--- thicket/stats/scoring.py
+++ thicket/stats/scoring.py
@@ -6,12 +6,9 @@ from .mean import mean
 from .std import std
 import math
 
-def _scoring_1(thicket, means, stds):
+def _scoring_1(means, stds, num_nodes):
 
     results = []
-
-    # Get number of nodes that we are scoring
-    num_nodes = len(thicket.dataframe.index.get_level_values(0).unique())
 
     for i in range(num_nodes):
         result = (means[0][1] - means[1][i])  * ( (stds[0][i] - stds[1][i]) / (np.abs(means[0][1] - means[1][i])))
@@ -19,12 +16,9 @@ def _scoring_1(thicket, means, stds):
     
     return results
 
-def _scoring_2(thicket, means, stds):
+def _scoring_2(means, stds, num_nodes):
 
     results = []
-
-    # Get number of nodes that we are scoring
-    num_nodes = len(thicket.dataframe.index.get_level_values(0).unique())
 
     for i in range(num_nodes):
         result = (means[0][1] - means[1][i]) + (stds[0][i] / means[0][i])  - (stds[1][i] / means[1][i])
@@ -32,12 +26,9 @@ def _scoring_2(thicket, means, stds):
     
     return results
 
-def _scoring_3(thicket, means, stds):
+def _scoring_3(means, stds, num_nodes):
 
     results = []
-
-    # Get number of nodes that we are scoring
-    num_nodes = len(thicket.dataframe.index.get_level_values(0).unique())
 
     for i in range(num_nodes):
         result = None
@@ -50,12 +41,9 @@ def _scoring_3(thicket, means, stds):
     
     return results
 
-def _scoring_4(thicket, means, stds):
+def _scoring_4(means, stds, num_nodes):
 
     results = []
-
-    # Get number of nodes that we are scoring
-    num_nodes = len(thicket.dataframe.index.get_level_values(0).unique())
 
     for i in range(num_nodes):
         result = None
@@ -69,18 +57,26 @@ def _scoring_4(thicket, means, stds):
     return results
 
 def score(thicket, columns, scoring_function):
+    
     if isinstance(columns, list) is False:
         raise ValueError(
                 "Columns must be a list!"
             )
 
-
-    # This is put in because of the fact that we are enforcing a columnar joined thicket
+    # Columns must be a tuples since we are dealing with columnar joined thickets
     for column in columns:
         if isinstance(column, tuple) is False:
             raise ValueError(
                 "Columns listed in columns must be a tuple!"
             )
+
+    if thicket.dataframe.columns.nlevels != 2:
+        raise ValueError(
+                "Thicket passed in must be a columnar joined thicket"
+            )
+
+    # Right now I have only two columns going in since we only have two compilers,
+    # But going forward I'm not sure we need this check. Or we can at least check if columns is at least 1
     if len(columns) != 2:
         raise ValueError(
             "Must specify two columns"
@@ -106,17 +102,35 @@ def score(thicket, columns, scoring_function):
     mean(thicket, columns)
     std(thicket, columns)
 
+    """
+        This is where I would put in the logic to group targets if thicket object has more than two targets.
+        example:
+            If thicket has Clang, Intel, and gcc
+            The scoring would be applied to all three as such:
+            Score (Clang, Intel), (Clang, gcc), (Intel, gcc)
+
+            All three would be appended to the statsframe
+
+            Need discussion on this!
+    """
+
     means[0] = thicket.statsframe.dataframe[(columns[0][0], "{}_mean".format(columns[0][1]))].to_list()
     means[1] = thicket.statsframe.dataframe[(columns[1][0], "{}_mean".format(columns[1][1]))].to_list()
     stds[0] = thicket.statsframe.dataframe[(columns[0][0], "{}_std".format(columns[0][1]))].to_list()
     stds[1] = thicket.statsframe.dataframe[(columns[1][0], "{}_std".format(columns[1][1]))].to_list()
     
-    #This is where we call the scoring function that the user specified!
-    resulting_scores = scoring_function(thicket, means, stds)
+    num_nodes = len(thicket.dataframe.index.get_level_values(0).unique())
 
+    #This is where we call the scoring function that the user specified!
+    resulting_scores = scoring_function(means, stds, num_nodes)
+    
+    # Statsframe column naming:
+    #   (Scoring, columnName_target1_target2_scoringFunctionName)
     stats_frame_column_name = ("Scoring", "{}_{}_{}_{}".format(columns[0][1], columns[0][0], columns[1][0], scoring_function.__name__))
-    # print(resulting_scores)
+
     thicket.statsframe.dataframe[stats_frame_column_name] = resulting_scores
+
+    return
 
 
 def scoring_1(thicket, columns):
