6bf694c7bd03377cd303eaa78144e8e72b614df2
Michael Richard Mckinsey <mckinsey@quartz386.llnl.gov>
2024-01-09 14:43:41 -0800

    Sort globs for deterministic file order

--- thicket/tests/conftest.py
+++ thicket/tests/conftest.py
@@ -111,7 +111,7 @@ def mpi_scaling_cali(data_dir, tmpdir):
 @pytest.fixture
 def rajaperf_cali_1trial(data_dir, tmpdir):
     """All tunings and variants for the first trial."""
-    cali_files = glob(f"{data_dir}/rajaperf-july-2023/**/1/*.cali", recursive=True)
+    cali_files = sorted(glob(f"{data_dir}/rajaperf-july-2023/**/1/*.cali", recursive=True))
     for cf in cali_files:
         shutil.copy(cf, str(tmpdir))
     return [os.path.join(str(tmpdir), f) for f in cali_files]
@@ -120,7 +120,7 @@ def rajaperf_cali_1trial(data_dir, tmpdir):
 @pytest.fixture
 def rajaperf_cali_alltrials(data_dir, tmpdir):
     """All tunings and variants."""
-    cali_files = glob(f"{data_dir}/rajaperf-july-2023/**/*.cali", recursive=True)
+    cali_files = sorted(glob(f"{data_dir}/rajaperf-july-2023/**/*.cali", recursive=True))
     for cf in cali_files:
         shutil.copy(cf, str(tmpdir))
     return [os.path.join(str(tmpdir), f) for f in cali_files]
@@ -129,10 +129,10 @@ def rajaperf_cali_alltrials(data_dir, tmpdir):
 @pytest.fixture
 def rajaperf_cuda_block128_1M_cali(data_dir, tmpdir):
     """All trials for CUDA block size 128 and problem size 1048576."""
-    cali_files = glob(
+    cali_files = sorted(glob(
         f"{data_dir}/rajaperf-july-2023/lassen/clang10.0.1_nvcc10.2.89_1048576/**/*block_128.cali",
         recursive=True,
-    )
+    ))
     for cf in cali_files:
         shutil.copy(cf, str(tmpdir))
     return [os.path.join(str(tmpdir), f) for f in cali_files]
@@ -141,10 +141,10 @@ def rajaperf_cuda_block128_1M_cali(data_dir, tmpdir):
 @pytest.fixture
 def rajaperf_seq_O3_1M_cali(data_dir, tmpdir):
     """All trials of for Base Sequential optimization level O3 and problem size 1048576."""
-    cali_files = glob(
+    cali_files = sorted(glob(
         f"{data_dir}/rajaperf-july-2023/quartz/gcc10.3.1_1048576/O3/**/Base_Seq-default.cali",
         recursive=True,
-    )
+    ))
     for cf in cali_files:
         shutil.copy(cf, str(tmpdir))
     return [os.path.join(str(tmpdir), f) for f in cali_files]
