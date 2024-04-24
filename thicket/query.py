c2bb7d0a6575a7611ed4ef189c02f3b99c10e982
Ian Lumsden <lumsden.ian@gmail.com>
2024-04-24 17:14:05 -0400

    Adds license header and flake8 control comment to query.py

--- thicket/query.py
+++ thicket/query.py
@@ -1,3 +1,11 @@
+# Copyright 2022 Lawrence Livermore National Security, LLC and other
+# Thicket Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: MIT
+
+# make flake8 unused names in this file.
+# flake8: noqa: F401
+
 from hatchet.query import (
     # New style queries
     # #################
