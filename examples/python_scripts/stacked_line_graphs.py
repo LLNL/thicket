4b52f7ef3abd955a59278ac8f1accc8b59b486e2
Senhe Hao <hsh@senhehao.com>
2024-05-15 10:25:31 -0700

    fixed correct file

--- examples/python_scripts/stacked_line_graphs.py
+++ examples/python_scripts/stacked_line_graphs.py
@@ -1,8 +1,13 @@
+import sys
+
+sys.path.append("/usr/gapps/spot/dev/hatchet-venv/x86_64/lib/python3.9/site-packages/")
+sys.path.append("/g/g20/hao3/thicket")
+sys.path.append("/usr/gapps/spot/dev/hatchet/x86_64/")
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import hatchet as ht
 import thicket as th
-import sys
 import copy
 import re
 import argparse
@@ -15,6 +20,7 @@ from hatchet import QueryMatcher
 def arg_parse():
     parser = argparse.ArgumentParser()
     parser.add_argument("--input_files", default="", type=str)
+    parser.add_argument("--groupby_parameter", default="", type=str)
     parser.add_argument("--filter_prefix", default="", type=str)
     parser.add_argument("--top_ten", default=False, type=bool)
     parser.add_argument("--out_graphs", nargs="+")
@@ -36,12 +42,14 @@ def make_graph(df, value, world_size, y_label):
     ax.set_xticklabels(x_labels)
     ax.legend(reversed(handles), reversed(labels), bbox_to_anchor=(1.1, 1.05))
 
+    plt.tight_layout()
+
     plt.savefig(value + ".png")
 
 
-def generate_plots(input_files, filter_prefix, top_ten, output_graphs):
-    tk = th.Thicket.from_caliperreader(glob(input_files, recursive=True))
-    gb = tk.groupby("mpi.world.size")
+def generate_plots(input_files, groupby_parameter, filter_prefix, top_ten, output_graphs):
+    tk = th.Thicket.from_caliperreader(glob(input_files+"/**/*.cali", recursive=True))
+    gb = tk.groupby(groupby_parameter)
 
     thickets = list(gb.values())
     world_size = list(gb.keys())
@@ -59,7 +67,7 @@ def generate_plots(input_files, filter_prefix, top_ten, output_graphs):
         filter_df = grouped_ctk
 
     if top_ten:
-        filter_df = filter_df.nlargest(10, [(8, "Total time")])
+        filter_df = filter_df.nlargest(10, [(world_size[0], "Total time")])
 
     for i in world_size:
         filter_df[i, "perc"] = (
@@ -74,4 +82,4 @@ def generate_plots(input_files, filter_prefix, top_ten, output_graphs):
 
 if __name__ == "__main__":
     args = arg_parse()
-    generate_plots(args.input_files, args.filter_prefix, args.top_ten, args.out_graphs)
+    generate_plots(args.input_files, args.groupby_parameter, args.filter_prefix, args.top_ten, args.out_graphs)
