f5f38a6e1ab321a9ce3e5c3067687e42b0f8dcf2
Senhe Hao <hsh@senhehao.com>
2024-06-24 15:53:10 -0700

    additional plotting options

--- examples/python_scripts/stacked_line_charts.py
+++ examples/python_scripts/stacked_line_charts.py
@@ -12,6 +12,7 @@ import hatchet as ht
 from hatchet import QueryMatcher
 from IPython.display import display, HTML
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import pandas as pd
 
 import thicket as th
@@ -23,9 +24,9 @@ def arg_parse():
         epilog="This script reads in Caliper files and generates stacked line charts based on the specified parameters.",
     )
     parser.add_argument("--input_files", required=True, type=str, help="Directory of Caliper file input, including all subdirectories.")
-    parser.add_argument("--x_axis_unique_metadata", required=True, type=str, help="Parameter that is varied during the experiment.")
-    parser.add_argument("--x_axis_scaling", default='linear', choices=['linear', 'log'], type=str, help="Scaling of x axis values for display on chart.")
-    parser.add_argument("--y_axis_metric", required=True, type=str, help="Metric to be visualized.")
+    parser.add_argument("--x_axis_unique_metadata", required=True, default='mpi.world.size', type=str, help="Parameter that is varied during the experiment.")
+    parser.add_argument("--x_axis_scaling", default=-1, type=int, help="Scaling of x axis values for display on chart.")
+    parser.add_argument("--y_axis_metric", required=True, default='Avg time/rank (exc)', type=str, help="Metric to be visualized.")
     parser.add_argument("--filter_prefix", default="", type=str, help="Optional: Filters only entries with prefix to be included in chart.")
     parser.add_argument("--group_nodes_name", default=True, type=bool, help="Optional: Specify if nodes with the same name are combined or not.")
     parser.add_argument("--top_n", default=-1, type=int, help="Optional: Filters only top n longest time entries to be included in chart.")
@@ -41,8 +42,14 @@ def arg_parse():
 def make_stacked_line_chart(df, value, world_size, title, xlabel, y_label, filename, x_axis_scaling):
     fig = plt.figure()
 
+    df.to_csv(filename + '.csv')
+
     tdf = df[[(i, value) for i in world_size]].T
     tdf.index = [int(re.sub(r"\D", "", str(item))) for item in tdf.index]
+
+    # Hard coded color map
+    color = ['#377eb8', '#ff7f00', '#4daf4a', '#f781bf', '#a65628', '#984ea3', '#999999', '#e41a1c', '#dede00']
+    mpl.rcParams['axes.prop_cycle'] = mpl.cycler(color=color)
 
     ax = tdf.plot(
         kind="area",
@@ -51,9 +58,9 @@ def make_stacked_line_chart(df, value, world_size, title, xlabel, y_label, filen
         ylabel=y_label,
     )
 
-    if x_axis_scaling == "log":
-        plt.xscale('log')
-    
+    if x_axis_scaling != -1:
+        ax.set_xscale('log', base=x_axis_scaling)
+
     ax.tick_params(axis='x', rotation=45)
     handles, labels = ax.get_legend_handles_labels()
     plt.xticks(tdf.index)
@@ -61,11 +68,15 @@ def make_stacked_line_chart(df, value, world_size, title, xlabel, y_label, filen
 
     plt.tight_layout()
 
-    plt.savefig(filename + ".png")
+    plt.savefig(filename + '.png')
 
 
 def process_thickets(input_files, x_axis_unique_metadata, y_axis_metric, filter_prefix, top_n, output_charts, additional_args):
     tk = th.Thicket.from_caliperreader(glob(input_files+"/**/*.cali", recursive=True))
+
+    f = open(additional_args.chart_file_name+'.txt', 'w')
+    f.write(tk.tree(metric_column=y_axis_metric))
+    f.close()
 
     gb = tk.groupby(x_axis_unique_metadata)
 
