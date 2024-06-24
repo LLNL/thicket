fc929c5cb44ebed472df729a0e90e73aed4f13b7
Michael McKinsey <michaelmckinsey@tamu.edu>
2024-06-24 14:29:43 -0500

    Remove print (#180)

--- thicket/thicket.py
+++ thicket/thicket.py
@@ -1383,9 +1383,6 @@ class Thicket(GraphFrame):
                 "The provided Thicket object has an empty metadata table."
             )
 
-        print(len(sub_thickets), " thickets created...")
-        print(sub_thickets)
-
         return GroupBy(by, sub_thickets)
 
     def filter_stats(self, filter_function):
