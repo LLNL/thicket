5944b4874dc5fd62a2722ce1af57d2cf27842c62
Michael McKinsey <michaelmckinsey1@gmail.com>
2024-07-05 16:54:11 -0500

    Shorten code

--- thicket/thicket.py
+++ thicket/thicket.py
@@ -936,17 +936,20 @@ class Thicket(GraphFrame):
         # Slices the DataFrame to simulate a single-level index
         try:
             # Select only columns used by tree for efficiency in _fill_perfdata
-            tree_cols = (
-                metric_column
-                if isinstance(metric_column, list)
-                else [metric_column]
-                + [
+            df_cols = [
+                col
+                for col in [
+                    *(
+                        metric_column
+                        if isinstance(metric_column, list)
+                        else [metric_column]
+                    ),
                     annotation_column,
                     name_column,
                     context_column,
                 ]
-            )
-            df_cols = [col for col in tree_cols if col in self.dataframe.columns]
+                if col in self.dataframe.columns
+            ]
             slice_df = self.dataframe.loc[:, df_cols]
             # _fill_perfdata to make sure number of nodes in df == graph
             slice_df = _fill_perfdata(slice_df)
