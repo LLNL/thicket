52b3c4aea7f25a69e5d47924db5553bc82cb9caa
Michael McKinsey <michaelmckinsey@tamu.edu>
2024-07-10 12:31:18 -0500

    Add Nodes in Slice Before Printing Tree (#181)
    * Fill perfdata to be able to print tree
    
    * Undo black change
    
    * Undo black change
    
    * Make copy of df
    
    * select only necessary columns
    
    * Shorten code
    
    * Shorten code
    
    * explicitly set default value
    
    ---------
    
    Co-authored-by: Michael Richard Mckinsey <mckinsey@quartz764.llnl.gov>
    Co-authored-by: Michael Richard Mckinsey <mckinsey@quartz1154.llnl.gov>

--- thicket/thicket.py
+++ thicket/thicket.py
@@ -28,6 +28,7 @@ from thicket.query import (
 import tqdm
 
 from thicket.ensemble import Ensemble
+from thicket.utils import _fill_perfdata
 
 try:
     from .ncu import NCUReader
@@ -934,8 +935,26 @@ class Thicket(GraphFrame):
         }
         # Slices the DataFrame to simulate a single-level index
         try:
+            # Select only columns used by tree for efficiency in _fill_perfdata
+            df_cols = [
+                col
+                for col in [
+                    *(
+                        metric_column
+                        if isinstance(metric_column, list)
+                        else [metric_column]
+                    ),
+                    annotation_column,
+                    name_column,
+                    context_column,
+                ]
+                if col in self.dataframe.columns
+            ]
+            slice_df = self.dataframe.loc[:, df_cols]
+            # _fill_perfdata to make sure number of nodes in df == graph
+            slice_df = _fill_perfdata(slice_df)
             slice_df = (
-                self.dataframe.loc[(slice(None),) + indices, :]
+                slice_df.loc[(slice(None),) + indices, :]
                 .reset_index()
                 .set_index("node")
             )
