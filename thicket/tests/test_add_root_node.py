cb5d2f858c203ba524971cb0e5668abd2392ff9e
Michael McKinsey <michaelmckinsey1@gmail.com>
2024-07-11 14:44:04 -0500

    update unit test

--- thicket/tests/test_add_root_node.py
+++ thicket/tests/test_add_root_node.py
@@ -11,12 +11,15 @@ def test_add_root_node(literal_thickets):
 
     assert len(tk.graph) == 4
 
+    # Call add_root_node
     tk.add_root_node({"name": "Test", "type": "function"})
-
+    # Get node variable
     test_node = tk.get_node("Test")
 
     # Check if node was inserted in all components
     assert isinstance(test_node, Node)
+    assert test_node._hatchet_nid == 3
+    assert test_node._depth == 0
     assert len(tk.graph) == 5
     assert len(tk.statsframe.graph) == 5
     assert test_node in tk.dataframe.index.get_level_values("node")
