18d8856ffef51eb282188583dcb1907cc14d4243
Michael Richard Mckinsey <mckinsey@ruby961.llnl.gov>
2024-07-29 15:51:27 -0700

    Enable matching RAJA_CUDA data using demangled string

--- thicket/ncu.py
+++ thicket/ncu.py
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: MIT
 
 from collections import defaultdict
+import re
 
 from hatchet import QueryMatcher
 import pandas as pd
@@ -63,7 +64,7 @@ class NCUReader:
         return query
 
     @staticmethod
-    def _read_ncu(thicket, ncu_report_mapping):
+    def _read_ncu(thicket, ncu_report_mapping, debug=False):
         """Read NCU report files and return dictionary of data.
 
         Arguments:
@@ -89,6 +90,10 @@ class NCUReader:
             profile_mapping_flipped = {v: k for k, v in thicket.profile_mapping.items()}
             ncu_hash = profile_mapping_flipped[ncu_report_mapping[ncu_report_file]]
 
+            # Relevant for kernel matching
+            variant = thicket.metadata.loc[ncu_hash, "variant"]
+            raja_cuda = variant.upper() == "RAJA_CUDA"
+
             # Load file
             report = ncu_report.load_report(ncu_report_file)
 
@@ -102,9 +107,7 @@ class NCUReader:
                 )
             elif num_ranges == 0:
                 raise ValueError(
-                    "NCU report file "
-                    + ncu_report_file
-                    + " has no ranges (no data)."
+                    "NCU report file " + ncu_report_file + " has no ranges (no data)."
                 )
             # Loop through ranges in report
             for range in report:
@@ -123,8 +126,10 @@ class NCUReader:
                 pbar = tqdm(range)
                 for i, action in enumerate(pbar):
                     pbar.set_description(f"Processing action {i}/{len(range)}")
-                    # Name of kernel
-                    kernel_name = action.name()
+                    # Demangled name of kernel
+                    demangled_kernel_name = action.name(
+                        ncu_report.IAction.NameBase_DEMANGLED
+                    )
                     # Get NCU-side kernel trace
                     kernel_call_trace = list(
                         action.nvtx_state().domain_by_id(0).push_pop_ranges()
@@ -134,9 +139,32 @@ class NCUReader:
                     if len(kernel_call_trace) == 0:
                         continue
                     else:
-                        call_trace_found=True
+                        call_trace_found = True
+
+                        # Call trace with last element removed
+                        # (last elem usually not useful for matching)
+                        temp_call_trace = kernel_call_trace[:-1]
+                        call_trace_str = "::".join([s.lower() for s in temp_call_trace])
+
+                        # Pattern ends with ":" if RAJA_CUDA, "<" if Base_CUDA
+                        kernel_pattern = rf"{call_trace_str}::(\w+)[<:]"
+                        kernel_match = re.search(kernel_pattern, demangled_kernel_name)
+                        kernel_str = kernel_match.group(1)
+
+                        if raja_cuda:
+                            # RAJA_CUDA variant
+                            instance_pattern = r"instance (\d+)"
+                            instance_match = re.findall(
+                                instance_pattern, demangled_kernel_name
+                            )
+                            instance_num = instance_match[-1]
+                            kernel_name = kernel_str + "_" + instance_num
+                        else:
+                            # Base_CUDA variant
+                            kernel_name = kernel_str
+
                         # Add kernel name to the end of the trace tuple
-                        kernel_call_trace.append(kernel_name)
+                        kernel_call_trace.append(kernel_str)
 
                         # Match ncu kernel to thicket node
                         matched_node = None
@@ -150,23 +178,45 @@ class NCUReader:
                             )
                             # Apply the query
                             node_set = query.apply(thicket)
-                            # Find the correct node
-                            matched_node = [
-                                n for n in node_set if kernel_name in n.frame["name"]
-                            ][0]
+                            # Find the correct node. This may also get the parent so we take the last one
+                            matched_nodes = [
+                                n
+                                for n in node_set
+                                if kernel_str in n.frame["name"]
+                                and (
+                                    f"#{instance_num}" in n.frame["name"]
+                                    if raja_cuda
+                                    else True
+                                )
+                            ]
+                            matched_node = matched_nodes[0]
 
-                        # matched_node should always exist at this point
-                        assert matched_node is not None
+                            if debug:
+                                if not raja_cuda:
+                                    instance_num = "NA"
+                                print(
+                                    f"Matched NCU kernel:\n\t{demangled_kernel_name}\nto Caliper Node:\n\t{matched_node}"
+                                )
+                                print(
+                                    f"AKA:\n\t{kernel_str} (instance {instance_num}) == {kernel_str} (#{instance_num})\n"
+                                )
+                                print("All matched nodes:")
+                                for node in matched_nodes:
+                                    print("\t", node)
+
                         # Set mapping
                         kernel_map[kernel_name] = matched_node
 
                         metric_values = [action[name].value() for name in metric_names]
+
                         assert len(metric_names) == len(metric_values)
                         data_dict[(matched_node, ncu_hash)].append(
                             dict(zip(metric_names, metric_values))
                         )
 
             if not call_trace_found:
-                raise ValueError(f"No kernel call traces found in {ncu_report_file}.\nCheck you are enabling the NVTX Caliper service when running NCU.")
+                raise ValueError(
+                    f"No kernel call traces found in {ncu_report_file}.\nCheck you are enabling the NVTX Caliper service when running NCU."
+                )
 
         return data_dict, rollup_dict
