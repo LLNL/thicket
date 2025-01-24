8567bc468b575649359ad24358376e4148edee5f
Dragana GRBIC <l1129498@pine-1.pine.cluster>
2025-01-23 19:44:55 -0600

    minnor fix

--- thicket/thicket.py
+++ thicket/thicket.py
@@ -404,6 +404,7 @@ class Thicket(GraphFrame):
             thicket = Thicket.reader_dispatch(
                 GraphFrame.from_hpctoolkit_latest,
                 intersection,
+                None,
                 disable_tqdm,
                 dirname,
                 directory_mapping=directory_mapping,
