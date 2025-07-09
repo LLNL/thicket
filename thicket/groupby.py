0ac864bc7a7674f1b10463a5075df920bf260146
Michael McKinsey <michaelmckinsey1@gmail.com>
2025-07-09 16:30:30 -0700

    Fix agg on single profile single column

--- thicket/groupby.py
+++ thicket/groupby.py
@@ -102,14 +102,18 @@ class GroupBy(dict):
         if (
             len(new_profile_label_mapping_df.columns) > 1
         ):  # Make tuple values if more than 1 column
-            new_profile_label_mapping_df = new_profile_label_mapping_df.apply(
+            new_profile_label_mapping_series = new_profile_label_mapping_df.apply(
                 tuple, axis=1
             )
-        else:  # Squeeze single column df into series
-            new_profile_label_mapping_df = new_profile_label_mapping_df.squeeze()
-        new_profile_label_mapping = (
-            new_profile_label_mapping_df.to_dict()
-        )  # Convert df to dict
+        else: # Squeeze single column df into series
+            new_profile_label_mapping_series = new_profile_label_mapping_df.squeeze()
+        # Single profile, single column squeeze results in single value, not series
+        if len(tk_c.profile) == 1:
+            new_profile_label_mapping = {tk_c.profile[0]: new_profile_label_mapping_series}
+        else:
+            new_profile_label_mapping = (
+                new_profile_label_mapping_series.to_dict()
+            )  # Convert df to dict
         new_profile = list(set(new_profile_label_mapping.values()))
         new_profile_mapping = defaultdict(list)
         for p in tk_c.profile:
